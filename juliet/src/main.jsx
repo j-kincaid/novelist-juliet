b9e8a3ef78ad23c15826db3660e1b1074a87871a
Jessica Kincaid <4j.kincaid@gmail.com>
2023-10-05 16:53:57 -0500

    add title and className to JSX expression

--- juliet/src/main.jsx
+++ juliet/src/main.jsx
@@ -1,11 +1,13 @@
 import { createRoot } from 'react-dom/client';
 
 const title = "Juliet Kincaid, Writer"
+const myTitleId = "main-title"
+const name = "Juliet Kincaid"
 const desc = "Mystery, Historical Fiction and Fantasy Novelist"
 const element = (
 <>
-<h1>{title}</h1>  
-<p>{desc}</p>
+<h1 id={myTitleId}>{name}, Writer</h1>  
+<p className="main-desc">{desc}</p>
 console.log(element);
 </>
 ) 
